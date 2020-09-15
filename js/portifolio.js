6abc0770df46325dd5e702b893be8d9365227784
Benjamin Nyakambangwe <benjaminnyakambangwe@gmail.com>
2020-09-15 14:51:51 +0200

    added a modal

--- js/portifolio.js
+++ js/portifolio.js
@@ -1,8 +1,14 @@
 const imgDiv = document.getElementById('image');
 const imgInnerDiv = document.getElementById('image-inner');
-	
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('myBtn');
+const span = document.getElementById('close');
+
+
 imgDiv.addEventListener('mouseover', displayOverlay);
 imgDiv.addEventListener('mouseout', removeOverlay);
+btn.addEventListener('click', openModal);
+span.addEventListener('click', closeModal);
 
 
 
@@ -21,4 +27,21 @@ function removeOverlay () {
 	}
 	
 	console.log('overlay off');
+}
+
+
+function openModal() {
+	modal.style.display = 'block';
+	console.log('modal open');
+}
+
+function closeModal() {
+	modal.style.display = 'none';
+	console.log('modal close');
+}
+
+window.onclick = function(e) {
+	if (e.target == modal) {
+		modal.style.display = 'none';
+	}
 }
